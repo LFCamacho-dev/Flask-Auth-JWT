92fe506738fa0df538e9913bf723d3265b1babc9
Luis Fernando Camacho <luisfernandoca@hotmail.com>
2022-03-04 23:06:03 -0600

    creating token properly

--- app.py
+++ app.py
@@ -8,12 +8,53 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '4a4e10e34613490c8bc2b3b1a93120f2'
 
 
+def token_required(func):
+    @wraps(func)
+    def decorated(*args, **kwargs):
+        token = request.args.get('token')
+        if not token:
+            return jsonify({'Alert!': 'Token is missing!'})
+        try:
+            payload = jwt.decode(token, app.config['SECRET_KEY'])
+        except:
+            return jsonify({'Alert!': 'Invalid Token!'})
+    return decorated
+
+
 @app.route('/')
 def home():
     if not session.get('logged_in'):
         return render_template('login.html')
     else:
         return "Logged in currently!"
+
+
+# for public
+@app.route('/public')
+def public():
+    return 'For Public'
+
+
+# for authenticated
+@app.route('/auth')
+@token_required
+def auth():
+    return "JWT is verified. Welcome to your dashboard"
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    if request.form['username'] and request.form['password'] == '123456':
+        session['logged_in'] = True
+
+        token = jwt.encode({
+            'user': request.form['username'],
+            'expiration': str(datetime.utcnow() + timedelta(seconds=60))
+        },
+            app.config['SECRET_KEY'])
+        return jsonify({'token': token})
+    else:
+        return make_response('Unable to verify', 403, {'WWW-Authenticate': 'Basic realm: "Authentication Failed "'})
 
 
 if __name__ == "__main__":
